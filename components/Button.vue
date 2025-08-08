88947b69aaff4df727810b3817eae16ce2f4c4d4
Mira <r1n0ku@yandex.ru>
2025-08-08 21:42:34 +0200

    fixed buttons

--- components/Button.vue
+++ components/Button.vue
@@ -7,16 +7,16 @@ const props = defineProps<ButtonProps>()
 </script>
 
 <template>
-  <div
-    class="bg-block border border-block-border md:px-8 px-5 py-2.5 w-fit rounded-md hover:opacity-85 hover:cursor-pointer transition-all duration-200 active:scale-95"
+  <NuxtLink
+    :to="props.to"
+    class="text-block-inner-text uppercase font-bold tracking-wider text-lg"
   >
-    <NuxtLink
-      :to="props.to"
-      class="text-block-inner-text uppercase font-bold tracking-wider text-lg"
+    <div
+      class="bg-block border border-block-border md:px-8 px-5 py-2.5 w-fit rounded-md hover:opacity-85 hover:cursor-pointer transition-all duration-200 active:scale-95"
     >
       <slot />
-    </NuxtLink>
-  </div>
+    </div>
+  </NuxtLink>
 </template>
 
 <style scoped></style>
